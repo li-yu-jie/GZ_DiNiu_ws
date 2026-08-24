07e6f25f623028d42a6648a859df0411bdabdf16
LYJ <3286149@qq.com>
2026-08-24 23:16:08 +0800

    fix(filter): 设定 x_max=3.80m，完美 100% 裁切 1.87m~2.58m 处车头前罩/油缸/门架所有同心圆环自遮挡红圈

--- src/diuniu_nav/launch/diuniu_mapping.launch.py
+++ src/diuniu_nav/launch/diuniu_mapping.launch.py
@@ -125,9 +125,9 @@ def generate_launch_description():
         name='laserscan_filter',
         parameters=[{
             'x_min': -1.65,
-            'x_max': 2.60,
-            'y_min': -0.55,
-            'y_max': 0.55,
+            'x_max': 3.80,
+            'y_min': -0.65,
+            'y_max': 0.65,
             'laser_x_offset': 1.215,
             'laser_y_offset': 0.0
         }],
